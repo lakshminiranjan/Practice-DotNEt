b9d644dd5fc81a0c4043cdbd315ef36cd57d1133
19BCE0590-Niranjan <lakshminiranjanp@gmail.com>
2025-02-26 21:08:37 +0530

    first commit

---
+++ Fibinocci/Program.cs
@@ -0,0 +1,53 @@
+﻿/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+    public int Fib(int n){
+        // int a,b,c,n,i;
+        // Console.WriteLine("Enter the number: ");
+        // n = Convert.ToInt32(Console.ReadLine());
+        
+        // a = 0;
+        // b = 1;
+        // Console.WriteLine(a + "\n" + b);
+        
+        // for(i = 1; i <= n; i++)
+        // {
+        //     c = a + b;
+        //     a = b;
+        //     b = c;
+        //     Console.WriteLine(c);
+        // }
+        int a = 0,b = 1;
+        if(n <= 0)
+            return -1;
+        else if(n == 1)
+            return 0;
+        else if(n == 2)
+            return 1;
+        for(int i= 2;i <= n; i++)
+        {
+            int c = a+b;
+            a = b;
+            b = c;
+            Console.WriteLine(c);
+        }
+        
+        return b;
+    }
+  static void Main(String[] args) {
+    // HelloWorld obj = new HelloWorld();
+    // obj.Fib();
+    HelloWorld obj = new HelloWorld();
+    Console.WriteLine("Enter the number: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int i = obj.Fib(n);
+    Console.WriteLine("The " + n + " th fibbinoci number is " + i);
+  }
+}
---
+++ SecondHighest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public void SecHighest()
+    {
+        int max1, max2;
+        int[] a = {78,75,14,84,56,94};
+        if(a[0] > a[1])
+        {
+            max1 = a[0];
+            max2 = a[1];
+        }
+        else
+        {
+            max1 = a[1];
+            max2 = a[0];
+        }
+        for(int i = 2; i < a.Length; i++)
+        {
+            if(a[i] > max1)
+            {
+                max2 = max1;  // Store the old maximum as second highest
+                max1 = a[i];  // Update the maximum
+            }
+            else if(a[i] > max2)
+            {
+                max2 = a[i];
+            }
+        }
+        Console.WriteLine(max2.ToString());
+    }
+    static void Main(string[] args)
+    {
+        Program obj = new Program();
+        obj.SecHighest();
+    }
+}
